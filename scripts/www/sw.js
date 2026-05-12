8c148294ce9df0a827e8bba53b7309a4f8ad48f4
lanbtcn <lanbtcn2@gmail.com>
2026-05-12 22:57:51 +0800

    update src file from pakeplus

---
+++ scripts/www/sw.js
@@ -0,0 +1,34 @@
+/* 高考化学通 - Service Worker */
+const CACHE_NAME = 'chem-app-v2';
+const CACHE_URLS = [
+  '/',
+  '/index.html',
+  '/styles/main.css',
+  '/js/data.js',
+  '/js/app.js',
+  '/manifest.json',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(CACHE_URLS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/index.html'))
+  );
+});
